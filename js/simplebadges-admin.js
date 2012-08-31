e4f85cd61fc58a02f72026dab37ece1a92a120e6
Ryan Imel <ryanimel@Ryan-Imels-MacBook-Air.local>
2012-08-30 22:09:51 -0400

    Hide the three options we don't need yet.

--- js/simplebadges-admin.js
+++ js/simplebadges-admin.js
@@ -1,16 +1,22 @@
 jQuery(document).ready(function($) {
 	
+	// Tweak the featured thumbnail metabox
+	$("#postimagediv h3.hndle span").text("Badge Image");
+	//$("#set-post-thumbnail").text("Set badge image");
+	$("#remove-post-thumbnail").text("Remove badge image");
+	
+	// Hide the three conditionals to begin with.
+	$( "#simplebadges_meta_box tr:eq(1), #simplebadges_meta_box tr:eq(2), #simplebadges_meta_box tr:eq(3)" ).hide();
+	
+	// Show the first conditional if automatic badges is checked.
+	
+		
 	// // Maybe I don't need CSS for now
 	// $("#simplebadges-meta-box tr:eq(1)").hide();
 	// $("#simplebadges-meta-box tr:eq(3)").hide();
 	// $("#simplebadges-meta-box tr:eq(4)").hide();
 	// 
 	
-	// Tweak the featured thumbnail metabox
-	$("#postimagediv h3.hndle span").text("Badge Image");
-	//$("#set-post-thumbnail").text("Set badge image");
-	$("#remove-post-thumbnail").text("Remove badge image");
-		
 	// Adjust the conditional dropdowns
 	// $("#simplebadges_badge_conditional_parttwo").appendTo("#simplebadges-meta-box tr:eq(2) td");
 	// $("#simplebadges_badge_conditional_partthree").appendTo("#simplebadges-meta-box tr:eq(2) td");
@@ -22,7 +28,7 @@ jQuery(document).ready(function($) {
 	// 	$("#simplebadges-meta-box tr:eq(2)").css( "display", "none" );
 	// }
 	// 
-	// $("#simplebadges-meta-box tr:eq(0) input:eq(1), #simplebadges-meta-box tr:eq(0) td label:eq(1)").click(function() {
+		// $("#simplebadges-meta-box tr:eq(0) input:eq(1), #simplebadges-meta-box tr:eq(0) td label:eq(1)").click(function() {
 	// 	$("#simplebadges-meta-box tr:eq(2)").css( "display", "table-row" );	
 	// });
 	// 
