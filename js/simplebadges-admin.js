1a0812ed391fb1126c6abe009000bdd0efd183e9
Ryan Imel <ryanimel@Ryan-Imels-MacBook-Air.local>
2012-08-30 22:22:30 -0400

    Variables for trigger and conditional rows in the js.
    That was fast. Simplified by not repeating quite so much. Set variables
    for the trigger metabox row and another for the conditionals that will
    pop up when the trigger is clicked. Saved a few characters.

--- js/simplebadges-admin.js
+++ js/simplebadges-admin.js
@@ -1,31 +1,32 @@
 jQuery(document).ready(function($) {
 	
-	
 	// Tweak the featured thumbnail metabox
 	$("#postimagediv h3.hndle span").text("Badge Image");
 	//$("#set-post-thumbnail").text("Set badge image");
 	$("#remove-post-thumbnail").text("Remove badge image");
 	
 	
+	// Set vars with the trigger row and the conditionals (that hide/show).
+	var $trigger = "#simplebadges_meta_box tr:eq(0) input:eq(2)";
+	var $conditionals = "#simplebadges_meta_box tr:eq(1), #simplebadges_meta_box tr:eq(2), #simplebadges_meta_box tr:eq(3)";
+	
 	// Show the conditionals if automatic badges is checked.
-	if ( $( "#simplebadges_meta_box tr:eq(0) input:eq(2)" ).is( ":checked" ) ) {
+	if ( $( $trigger ).is( ":checked" ) ) {
 		
-		$( "#simplebadges_meta_box tr:eq(1), #simplebadges_meta_box tr:eq(2), #simplebadges_meta_box tr:eq(3)" ).css( "display", "table-row" );
+		$( $conditionals ).css( "display", "table-row" );
 		
 	} else {
 		
 		// Hide the three conditionals to begin with.
-		$( "#simplebadges_meta_box tr:eq(1), #simplebadges_meta_box tr:eq(2), #simplebadges_meta_box tr:eq(3)" ).hide();
+		$( $conditionals ).hide();
 		
 	}
 	
-	
 	// Pop options open if the checkbox is checked.
-	$( "#simplebadges_meta_box tr:eq(0) input:eq(2)" ).click( function(){
+	$( $trigger ).click( function(){
 
-		$( "#simplebadges_meta_box tr:eq(1), #simplebadges_meta_box tr:eq(2), #simplebadges_meta_box tr:eq(3)" ).toggle();
+		$( $conditionals ).toggle();
 		
 	} );
 	
-
 });
