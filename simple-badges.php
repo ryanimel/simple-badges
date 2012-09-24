c69c03700950484334234b7e2f55a0079b6baf12
Ryan Imel <ryan@aspiringindie.com>
2012-09-24 05:34:24 -0400

    Moved listener into Simple Badges plugin.

--- simple-badges.php
+++ simple-badges.php
@@ -693,3 +693,29 @@ function rwi_simplebadges_admin_dashboard_footer() {
 
 }
 
+
+/**
+ * Badge award listener for Simple Points
+ */
+add_filter( 'simple_points_helper', 'rwi_user_badge_count', 10, 1 );
+function rwi_user_badge_count( $array ) {
+	$array[] = array(
+		'id'	=> 'rwi_user_badge_count',
+		'name'	=> 'User badge count',
+		'value'	=> 'user_badge_count',
+		'type'	=> 'decimal'
+	);
+	
+	return $array;
+}
+
+add_action( 'simplebadges_after_adding', 'rwi_user_badge_count_action', 10, 2 );
+function rwi_user_badge_count_action( $badge_id, $user_id ) {
+	global $SimpleCondition;
+	
+	$user_badges = get_user_meta( $user_id, 'simplebadges_badges' );
+	$count = count( $user_badges );
+	
+	$SimpleCondition->check( 'rwi_user_badge_count', $user_id, $count );
+}
+
